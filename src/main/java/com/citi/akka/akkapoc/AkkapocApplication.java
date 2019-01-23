5dcd27e0fe2f826bc415a496a870e3a8c620f4d4
Raghavender Kotla <raghavenderkotla@Raghavenders-MacBook-Pro.local>
2019-01-23 06:55:49 -0500

    initial commit

---
+++ src/main/java/com/citi/akka/akkapoc/AkkapocApplication.java
@@ -0,0 +1,14 @@
+package com.citi.akka.akkapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkapocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkapocApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/citi/akka/akkapoc/AkkapocApplicationTests.java
@@ -0,0 +1,17 @@
+package com.citi.akka.akkapoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AkkapocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
